3470cb4b6f0a30946e287203c3931a2a5174e3c3
Roman Syvash <syvashroman0109@gmail.com>
2024-02-13 21:24:09 +0200

    first commit

---
+++ apps/main/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request, 'main/index.html')
+
+def contacts(request):
+    return render(request,'main/contacts.html')  
+
+def about(request):
+    return render(request,'main/about_us.html' )
+
+def navig(request):
+    return render(request, 'main/navigation.html')
